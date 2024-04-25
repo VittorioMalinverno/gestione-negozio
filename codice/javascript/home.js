573babad60bf8773743c8488c18cdb03a839be91
ciroandrea <strazzullociroandrea@itis-molinari.eu>
2024-04-25 15:38:18 +0200

    Sistemazione url

--- codice/javascript/home.js
+++ codice/javascript/home.js
@@ -1,13 +1,22 @@
-import {recuperaProdotti} from "./viewArticoli.js";
+import { recuperaProdotti } from "./viewArticoli.js";
 const articoli = document.getElementById("articoli");
 const cardTemplate = `
-
+<div class="col mx-5 my-5">
+          <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title mb-4">%TITOLO</h5>
+              <img src="./placeholder.png" class="card-img-top" alt="...">
+              <div class="d-flex mt-5 justify-content-between align-items-center">
+                <p class="m-0">%PREZZO €</p>
+                <a class="btn btn-primary rounded-pill acquista" id="%ID">Acquista</a>
+              </div>
+            </div>
+          </div>
+        </div>
 `;
-
-window.onload = async() =>{
+const view = async() =>{
     let html = "";
     const rsp = await recuperaProdotti();
-    console.log(rsp);
     rsp.result.forEach(element=>{
         html += cardTemplate
         .replace("%TITOLO", element.nome)
@@ -17,7 +26,10 @@ window.onload = async() =>{
     articoli.innerHTML = html;
     document.querySelectorAll(".acquista").forEach(element =>{
         element.onclick = () =>{
-            window.location.href="../php/pagine/prodotto.php?idProdotto="+element.id;
+            window.location.href="./prodotto.php?idProdotto="+element.id;
         }
     })
 }
+
+
+view();
--- codice/javascript/viewArticoli.js
+++ codice/javascript/viewArticoli.js
@@ -1,7 +1,7 @@
 //fetch per recuperare tutti i prodotti presenti nel db
 export const recuperaProdotti = () => {
     return new Promise((resolve, reject) => {
-        fetch("../php/servizi/viewArticoli.php", {
+        fetch("../servizi/viewArticoli.php", {
             method: "Post",
             headers: {
                 "content-type": "application/json",
@@ -18,7 +18,7 @@ export const recuperaProdotti = () => {
 
 export const recuperaQuantitaVenduta = ()=>{
     return new Promise((resolve, reject) => {
-        fetch("../php/servizi/prodottoVenduto.php", {
+        fetch("../servizi/prodottoVenduto.php", {
             method: "Post",
             headers: {
                 "content-type": "application/json",
